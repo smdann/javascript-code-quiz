ac94218d46eefa38fc1226df808ad704c8deed59
SDann <smdann17@gmail.com>
2022-09-19 17:51:50 -0500

    In JavaScript: added an event listener to listen for click on start quiz button. Created a countdown function, which is started upon clicking the start quiz button, and displays the remaining time. Edited a few id names in HTML and CSS files.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,35 @@
-// Code Quiz
+var startQuizEl = document.querySelector("#start");
+var timerEl = document.getElementById("time-left");
+
+// Timer Function
+function countdown() {
+    var timeLeft = 60;
+    var timeInterval = setInterval(function() {
+        timeLeft--;
+        timerEl.textContent = timeLeft + " seconds remaining";
+        if(timeLeft === 0) {
+            clearInterval(timeInterval);
+            console.log("timer is running")
+        }
+    }, 1000);
+}
 
 // Click start button
     // Add event listener to Start Quiz button
-
+startQuizEl.addEventListener("click", function(event) {
+    console.log("start quiz")
     // If Start button is clicked, timer starts counting down
+    if (event.target.getAttribute("id") === "start") {
+        console.log("this worked");
+        countdown();
+        
+    }  
+})
+
+
+    
         // and a question is presented
+
 
     // Question will display as question in heading
         // Multiple choice answers display as buttons
