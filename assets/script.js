3948a5cea02ddf57accfbff48e49df611f1b00ca
SDann <smdann17@gmail.com>
2022-09-19 16:48:33 -0500

    Added initial quiz information to HTML file. Added flexbox layout and styling to CSS file. Started pseudocoding in JavaScript file.

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+// Code Quiz
+
+// Click start button
+    // Add event listener to Start Quiz button
+
+    // If Start button is clicked, timer starts counting down
+        // and a question is presented
+
+    // Question will display as question in heading
+        // Multiple choice answers display as buttons
+
+        // Each button will have the same event listener
+
+        // The event attribute that is equal to the correct answer will cause a message that says "Correct" to display.
+
+        // Answer question correctly
+            // Presented a new question
+        
+        // Event attributes that do not equal the correct answer will cause a message that says "Incorrect" to display.
+            // Time is subtracted from the clock
+            // Presented a new question
+
+    // When all questions are answered or the timer reaches 0
+        // Game is over
+        // Enter initials 
+            // Submits score to score board
