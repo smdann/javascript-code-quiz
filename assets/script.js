39d33000eef6140fc84c64d8fd77369a18b3963c
SDann <smdann17@gmail.com>
2022-09-20 16:12:27 -0500

    Added an array (questions) of objects (questions with answers).

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,40 @@
+// Document selectors
 var startQuizEl = document.querySelector("#start");
 var timerEl = document.getElementById("time-left");
+var currentScore = 0;
 var h1Tags = document.querySelector(".h1-tag");
 var pToButtons = document.querySelector("#para");
 
 // Question Variables
-var ques1 = "Commonly used data types DO NOT include:"
+var h1Initial =  "JavaScript Coding Quiz"
 
+
+var questions = [
+    // Question 1
+    {   
+      question: "Commonly used data types DO NOT include:",
+      answers: [
+        "strings",
+        "booleans",
+        "alerts",
+        "numbers"
+      ]      
+    },
+    // Question 2
+    {
+      question: "The condition in an if / else statement is enclosed within ______.",
+      answers: [
+        "quotes",
+        "curly brackets",
+        "parentheses",
+        "square brackets"
+      ]
+    }
+  ]
 
 // Timer Function
 function countdown() {
-    var timeLeft = 60;
+    var timeLeft = 120;
     var timeInterval = setInterval(function() {
         timeLeft--;
         timerEl.textContent = timeLeft + " seconds remaining";
@@ -20,12 +45,17 @@ function countdown() {
 }
 
 // Question Function
-function question() {
-    h1Tags.replaceChildren("Commonly used data types DO NOT include:");
-    pToButtons.replaceChildren("1. Strings");
+function showQuestion() {
+    var currentQuestionObj = questions[i];
+    var section = document.createElement("section");
+    var h2Tag = document.createElement("h2");
+    var ulTag = document.createElement("ul");
+    console.log (document)
     
 
 }
+
+
 
 // Click start button
     // Add event listener to Start Quiz button
@@ -33,16 +63,16 @@ startQuizEl.addEventListener("click", function(event) {
     console.log("start quiz")
     // If Start button is clicked, timer starts counting down
     if (event.target.getAttribute("id") === "start") {
-        console.log("this worked");
         countdown();
-        question();
+        // and a question is presented
+        showQuestion(questions[0]);
         
     }  
 })
 
 
     
-        // and a question is presented
+        
 
 
     // Question will display as question in heading
@@ -63,3 +93,6 @@ startQuizEl.addEventListener("click", function(event) {
         // Game is over
         // Enter initials 
             // Submits score to score board
+
+    
+    // Questions will be held as an array of objects (with questions and answers)
