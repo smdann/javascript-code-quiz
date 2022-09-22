d4150ac80f9f44a946634f871c3e19f282061f91
SDann <smdann17@gmail.com>
2022-09-22 08:48:36 -0500

    In HTML file: added a new div in main section to contain text prior to hitting start button. Added a div to hold the question and answer buttons to display after hitting start. In JavaScript file, separated answer choices into individual items within the object. Update showQuestion function to reflect changes made to questions array. Created a checkAnswer function with if/else statement to decide what happens when a user makes an answer selection. Also removed individual button event listeners and created a new one that applies to all of the answer buttons.

--- assets/script.js
+++ assets/script.js
@@ -2,23 +2,56 @@
 var startQuizEl = document.querySelector("#start");
 var timerEl = document.getElementById("time-left");
 var mainSection = document.querySelector("main");
+var questionContainer = document.querySelector(".questionContainer");
+var questionDiv = document.querySelector("#question");
+var buttonGrid = document.querySelector("#btngrid");
+var startContainer = document.querySelector(".startContainer")
+var timeLeft = 120;
+var timeInterval;
+var score = 0;
+var finalScore;
 
-var currentScore = 0;
 
 
-// Array of questions containing individual questions with answer choices
+
+
+// Array of objects (question, answer choices, and a correct answer)
 var questions = [
     // Question 1
     {   
       question: "Commonly used data types DO NOT include:",
-      answers: ["strings", "booleans", "alerts", "numbers"],
+      answer1: "strings", 
+      answer2: "booleans", 
+      answer3: "alerts", 
+      answer4: "numbers",
       correct: "alerts"      
     },
     // Question 2
     {
       question: "The condition in an if / else statement is enclosed within ______.",
-      answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer1: "quotes", 
+      answer2:"curly brackets", 
+      answer3:"parentheses", 
+      answer4:"square brackets",
       correct: "parentheses"
+    },
+    // Question 3
+    {
+      question: "Commonly used data types DO NOT include:",
+      answer1: "strings", 
+      answer2: "booleans", 
+      answer3: "alerts", 
+      answer4: "numbers",
+      correct: "alerts"  
+    },
+    // Question 4
+    {
+      question: "Commonly used data types DO NOT include:",
+      answer1: "strings", 
+      answer2: "booleans", 
+      answer3: "alerts", 
+      answer4: "numbers",
+      correct: "alerts" 
     }
   ]
 
@@ -29,34 +62,33 @@ var questions = [
 
 // Timer Function
 function countdown() {
-    var timeLeft = 120;
-    var timeInterval = setInterval(function() {
+    
+    timeInterval = setInterval(function() {
         timeLeft--;
-        timerEl.textContent = timeLeft + " seconds remaining";
+        timerEl.textContent = timeLeft;
         if(timeLeft === 0) {
             clearInterval(timeInterval);
+            // Finished
+            timerEl.textContent = "Time's Up!";
         }
     }, 1000);
 }
 
 
 // Tells the function which question to use from the questions array
-var questionNumber = questions[0];
+var questionNumber = 0;
 
 // Question Function
 function showQuestion() {
+    if (questionNumber === questions.length){
+      endQuiz()
+    }
     // Clear all elements in main
-    mainSection.innerHTML = "";
+    startContainer.classList.add("hide")
     
-    // Loop through the questions array
-    //  for (var i = 0; i < questions.length; i++){
-      currentQuestion = questionNumber;
-      
       
       // Creates elements that will house question data
-      var section = document.createElement("section");
       var h2Tag = document.createElement("h2");
-      var buttonDiv = document.createElement("div")
       var button1 = document.createElement("button");
       var button2 = document.createElement("button");
       var button3 = document.createElement("button");
@@ -64,78 +96,123 @@ function showQuestion() {
       var answerFeedback = document.createElement("h4");
 
       // Adds questions data to each element
-      h2Tag.textContent = currentQuestion.question;
-      button1.textContent = currentQuestion.answers[0];
-      button2.textContent = currentQuestion.answers[1];
-      button3.textContent = currentQuestion.answers[2];
-      button4.textContent = currentQuestion.answers[3];
+      h2Tag.textContent = questions[questionNumber].question;
+      button1.textContent = questions[questionNumber].answer1;
+      button2.textContent = questions[questionNumber].answer2;
+      button3.textContent = questions[questionNumber].answer3;
+      button4.textContent = questions[questionNumber].answer4;
       
+      button1.setAttribute("value", questions[questionNumber].answer1)
+      button2.setAttribute("value", questions[questionNumber].answer2)
+      button3.setAttribute("value", questions[questionNumber].answer3)
+      button4.setAttribute("value", questions[questionNumber].answer4)
       // Adds each element to the DOM
-      document.body.appendChild(section);
-      document.body.appendChild(h2Tag);
-      document.body.appendChild(buttonDiv);
-      document.body.appendChild(button1);
-      document.body.appendChild(button2);
-      document.body.appendChild(button3);
-      document.body.appendChild(button4);
+      
+      questionDiv.appendChild(h2Tag);
+      buttonGrid.append(button1, button2, button3, button4);
+      
 
-      // Adds an event listener to each button. Compares the text of the answer choice selected to the text of the correct answer. Provides feedback.
-      button1.addEventListener("click", function(event){
-        if ((event.target.textContent) == (questions[0].correct)){
-          answerFeedback.textContent = ("Correct!");
-          document.body.appendChild(answerFeedback);
-        } else {
-          answerFeedback.textContent = ("Incorrect");
-          document.body.appendChild(answerFeedback);
-        }
-      })
-      button2.addEventListener("click", function(){
-        if ((event.target.textContent) == (questions[0].correct)){
-          answerFeedback.textContent = ("Correct!");
-          document.body.appendChild(answerFeedback);
-        } else {
-          answerFeedback.textContent = ("Incorrect");
-          document.body.appendChild(answerFeedback);
-        }
-      })
-      button3.addEventListener("click", function(){
-        if ((event.target.textContent) == (questions[0].correct)){
-          answerFeedback.textContent = ("Correct!");
-          document.body.appendChild(answerFeedback);
-        } else {
-          answerFeedback.textContent = ("Incorrect");
-          document.body.appendChild(answerFeedback);
-        }
-      })
-      button4.addEventListener("click", function(){
-        if ((event.target.textContent) == (questions[0].correct)){
-          answerFeedback.textContent = ("Correct!");
-          document.body.appendChild(answerFeedback);
-        } else {
-          answerFeedback.textContent = ("Incorrect");
-          document.body.appendChild(answerFeedback);
-        }
-      })
-
-     
-     
-}
+      // Adds an event listener to each button. 
+      // Compares the text of the answer choice selected to the text of the correct answer. 
+      // Provides feedback.
+      // Time deduction for incorrect answers
+      // button1.addEventListener("click", function(event){
+      //   if ((event.target.textContent) == (questions[0].correct)){
+      //     answerFeedback.textContent = ("Correct!");
+      //     document.body.appendChild(answerFeedback);
+      //   } else {
+      //     answerFeedback.textContent = ("Incorrect");
+      //     document.body.appendChild(answerFeedback);
+      //     timerEl.textContent = timerEl.textContent - 10;
+      //     console.log(timerEl.textContent)
+      //   }
+      // })
+      // button2.addEventListener("click", function(event){
+      //   if ((event.target.textContent) == (questions[0].correct)){
+      //     answerFeedback.textContent = ("Correct!");
+      //     document.body.appendChild(answerFeedback);
+      //   } else {
+      //     answerFeedback.textContent = ("Incorrect");
+      //     document.body.appendChild(answerFeedback);
+      //   }
+      // })
+      // button3.addEventListener("click", function(event){
+      //   if ((event.target.textContent) == (questions[0].correct)){
+      //     answerFeedback.textContent = ("Correct!");
+      //     document.body.appendChild(answerFeedback);
+      //   } else {
+      //     answerFeedback.textContent = ("Incorrect");
+      //     document.body.appendChild(answerFeedback);
+      //   }
+      // })
+      // button4.addEventListener("click", function(event){
+      //   if ((event.target.textContent) == (questions[0].correct)){
+      //     answerFeedback.textContent = ("Correct!");
+      //     document.body.appendChild(answerFeedback);
+      //   } else {
+      //     answerFeedback.textContent = ("Incorrect");
+      //     document.body.appendChild(answerFeedback);
+      //   }
+      // } 
+      //  )
+} 
   
+// Answer question correctly
+  // Present a new question
 
+// Answer question incorrectly
+  // Time deduction and present new question
+
+// When all questions are answered or the timer reaches 0
+  // Quiz is over
+  // Enter initials 
+  // Submit score to score board
+
+// Highscores will be stored in local storage (initials and score)
+
+// Highscores will be retrieved from local storage and displayed on the screen
    
+function checkAnswer (userChoice){
+  if (userChoice === questions[questionNumber].correct){
+    buttonGrid.innerHTML = ""
+    questionNumber ++;
+    score ++;
+    console.log(score)
+    showQuestion()
+  } else {
+    buttonGrid.innerHTML = ""
+    questionNumber ++;
+    timeLeft = timeLeft - 10;
+    console.log(timeLeft)
+    showQuestion()
+  }
+}
 
+function endQuiz (){
+
+}
 
 // Click start button
     // Add event listener to Start Quiz button
 startQuizEl.addEventListener("click", function(event) {
     console.log("start quiz")
     // If Start button is clicked, timer starts counting down
-    if (event.target.getAttribute("id") === "start") {
         countdown();
         // and a question is presented
         showQuestion();
         
         
 
-}
+
 })
+
+buttonGrid.addEventListener("click", () => {
+  var buttonClick = this.event.target.value
+  console.log(buttonClick)
+  checkAnswer(buttonClick)
+})
+
+
+// highScoreArr=JSON.parse(localStorage.getItem("highScores")) || []
+
+// highScoreArr=JSON.parse(localStorage.getItem("highScores")) || []
